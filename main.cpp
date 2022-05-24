77c8a6cff172f4231c3d630ce7e5451a6bf2b181
KubaMilion <jakub.grzegorczyk@gmail.com>
2022-05-24 23:34:14 +0200

    clean

--- main.cpp
+++ main.cpp
@@ -14,10 +14,7 @@ int count_banans(int * edge_tab,int s){
             if(temp==edge_tab[j]) counter=counter+1;
         }
         if(counter==1) output=output+1;
-
-
      }
-     cout<<"mamy "<<output<<" bananow";
 return output;
 
 }
@@ -28,8 +25,6 @@ int count_banans_single(int * edge_tab,int s,int num){
         for(int j=0;j<s;j++){
             if(num==edge_tab[j]) output=output+1;
         }
-
-     cout<<"tutaj dla num "<<num<<"waga to "<<output<<endl;
 
 return output;
 
@@ -47,11 +42,10 @@ struct Edge{
   int y_lvl;
 };
 
-void is_banan_tree(int * edge_tab2,int s2){
+bool is_banan_tree(int * edge_tab2,int s2, int counted_bananas){
+    bool output=true;
     // tutaj wyznaczamy tablice z wierzcho³kami ktore moga byc bananami
-    int dim=count_banans(edge_tab2,s2);
-    cout<<"banans counter "<<dim;
-    int * bananas=new int[dim];
+    int * bananas=new int[counted_bananas];
     int counter_b=0;
     for(int i=0;i<s2/2;i++){
         if(count_banans_single(edge_tab2,s2,i)==1){
@@ -62,11 +56,6 @@ void is_banan_tree(int * edge_tab2,int s2){
 
 
 
-
-    cout<<"banany ---------------------------------"<<endl;
-    for(int i=0;i<dim;i++){
-        cout<<bananas[i]<<endl;
-    }
 
 
     int **points_arr = new int*[s2];
@@ -88,8 +77,6 @@ void is_banan_tree(int * edge_tab2,int s2){
 
 
     }
-    cout<<"==================================="<<endl;
-
 
     int weight=0;
 
@@ -122,22 +109,10 @@ while(weight<s2/4){
 }
 
 
-   for(int i=0;i<s2;i++){
-
-        cout<<"tutaj printujemuy strcut"<<endl;
-
-        cout<<points_arr[i][0]<<endl;
-        cout<<points_arr[i][1]<<endl;
-
-
-
-    }
-
     string * points_array=new string [s2];
 
        for(int i=0;i<s2;i++){
 
-        cout<<"tutaj printujemuy strcut"<<endl;
         points_array[i]=to_string(points_arr[i][0])+"-"+to_string(points_arr[i][1]);
 
        }
@@ -151,14 +126,8 @@ while(weight<s2/4){
         sort(points_vec.begin(),points_vec.end());
         points_vec.erase(unique(points_vec.begin(),points_vec.end()),points_vec.end());
 
-        cout<<"VECTOR==========================="<<endl;
-
-        for(int i=0;i<points_vec.size();i++){
-
-            cout<<points_vec[i]<<endl;
-        }
         int new_size=points_vec.size();
-        graphEdge graphEdge_arr [new_size];
+        graphEdge * graphEdge_arr =new graphEdge[new_size];
 
         for(int i=0;i<new_size;i++){
             string str=points_vec[i];
@@ -167,11 +136,6 @@ while(weight<s2/4){
             graphEdge_arr[i].weight=stoi(str.substr(pos+1));
         }
 
-        for(int i=0;i<new_size;i++){
-
-            cout<<graphEdge_arr[i].ver<<" ";
-            cout<<graphEdge_arr[i].weight<<endl;
-        }
 
         //wyznaczam root
          int root=0;
@@ -184,14 +148,12 @@ while(weight<s2/4){
 
         }
 
-        cout<<"root ma level "<<root<<" dla "<<graphEdge_arr[idx].ver;
         int root_point=graphEdge_arr[idx].ver;
         //"droga" dla pierwszego banana do root
         int route=0;
 
-        Edge edges [s2/2];
+        Edge * edges =new Edge[s2/2];
         int itr=0;
-            cout<<endl;
         for(int i=0;i<s2;i++){
             edges[itr].x=edge_tab2[i];
             for(int j=0;j<new_size;j++){
@@ -211,10 +173,10 @@ while(weight<s2/4){
         }
 
 
-        for(int k=0;k<dim;k++){
+        for(int k=0;k<counted_bananas;k++){
             int connector=bananas[k];
             int route_counter=0;
-            int max_itr=graphEdge_arr[idx].weight+1;
+            int max_itr=graphEdge_arr[idx].weight;
             while(max_itr>0){
                 for(int i=0;i<s2/2;i++){
                     if(edges[i].x==connector && edges[i].y_lvl>route_counter) {connector=edges[i].y;route_counter=edges[i].y_lvl;}
@@ -225,41 +187,59 @@ while(weight<s2/4){
                 max_itr--;
             }
 
-            if(route_counter==graphEdge_arr[idx].weight) {cout<<"Jest ok"<<endl;}
-            else {cout<<"NIE JEST OK!!!!!!11"<<endl;}
+            if(route_counter==graphEdge_arr[idx].weight) {output=true;}
+            else {output=false;break;}
         }
+        return output;
 
 
 }
 
 
+    struct Data{
+        int x;
+        string text;
+
+    };
 int main()
 {
-    string init_text;
-    int n;
 
-    cin>>n;
-    cin>>init_text;
-    string newinput=init_text;
+    int lines;
+    cin>>lines;
+    Data * data=new Data[lines];
+    for(int i=0;i<lines;i++){
+        int x_temp;
+        string text_temp;
+        cin>>x_temp;
+        cin>>text_temp;
 
-    int tab[n][n];
+        data[i].x=x_temp;
+        data[i].text=text_temp;
 
+    }
 
+ for(int i=0;i<lines;i++){
+    string init_text=data[i].text;
+    string newinput=data[i].text;
+    int n=data[i].x;
+    int ** tab=new int *[n];
+
+    for(int i = 0; i < n; ++i){
+        tab[i] = new int[n];
+    }
     char ch = '1';
 
     int m = count(init_text.begin(), init_text.end(), ch);
 
-    int edge_tab [m];
+    int * edge_tab=new int[m];
 
     int counter=1;
     string check="";
         for (int i = 0; i < n; i++) {
-            cout<<"Wiersz nr "<<i<<" ";
             for(int j=0;j<n;j++){
 
                 int num=newinput.at(j)-48;
                 tab[i][j]=num;
-                cout<<tab[i][j]<<" ";
                 if(tab[i][j]==1){
                     if(j>i){
                         string temp2=to_string(i)+" "+to_string(j)+",";
@@ -296,25 +276,25 @@ int main()
                 }
 
             }
-            cout<<endl;
 
     }
 
-    cout<<"sprawdzamy vector"<<endl;
 
-    for(int i=1;i<=m;i++){
 
-        cout<<edge_tab[i-1]<<" "<<edge_tab[i]<<endl;
-        i++;
+
+    int bananas=count_banans(edge_tab,m);
+
+    if(is_banan_tree(edge_tab,m,bananas)){
+         cout<<bananas<<" bananas :)"<<endl;
+         }
+    else{
+        cout<<"0 bananas :("<<endl;
+
     }
 
-    cout<<"========================================="<<endl;
-
-
-    count_banans(edge_tab,m);
-    is_banan_tree(edge_tab,m);
 
 
 
+ }
     return 0;
 }
