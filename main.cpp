d2a85d02f266aea56725a898b5badd1966972ca1
KubaMilion <jakub.grzegorczyk@gmail.com>
2022-05-24 22:51:15 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -40,8 +40,33 @@ struct graphEdge {
     int ver, weight;
 
 };
+struct Edge{
+  int x;
+  int x_lvl;
+  int y;
+  int y_lvl;
+};
 
 void is_banan_tree(int * edge_tab2,int s2){
+    // tutaj wyznaczamy tablice z wierzcho³kami ktore moga byc bananami
+    int dim=count_banans(edge_tab2,s2);
+    cout<<"banans counter "<<dim;
+    int * bananas=new int[dim];
+    int counter_b=0;
+    for(int i=0;i<s2/2;i++){
+        if(count_banans_single(edge_tab2,s2,i)==1){
+            bananas[counter_b]=i;
+            counter_b=counter_b+1;
+        }
+    }
+
+
+
+
+    cout<<"banany ---------------------------------"<<endl;
+    for(int i=0;i<dim;i++){
+        cout<<bananas[i]<<endl;
+    }
 
 
     int **points_arr = new int*[s2];
@@ -147,6 +172,64 @@ while(weight<s2/4){
             cout<<graphEdge_arr[i].ver<<" ";
             cout<<graphEdge_arr[i].weight<<endl;
         }
+
+        //wyznaczam root
+         int root=0;
+         int idx=0;
+        for(int i=0;i<new_size;i++){
+            if(graphEdge_arr[i].weight>root){
+                root=graphEdge_arr[i].weight;
+                idx=i;
+            }
+
+        }
+
+        cout<<"root ma level "<<root<<" dla "<<graphEdge_arr[idx].ver;
+        int root_point=graphEdge_arr[idx].ver;
+        //"droga" dla pierwszego banana do root
+        int route=0;
+
+        Edge edges [s2/2];
+        int itr=0;
+            cout<<endl;
+        for(int i=0;i<s2;i++){
+            edges[itr].x=edge_tab2[i];
+            for(int j=0;j<new_size;j++){
+                if(edges[itr].x==graphEdge_arr[j].ver) edges[itr].x_lvl=graphEdge_arr[j].weight;
+            }
+
+
+            edges[itr].y=edge_tab2[i+1];
+            for(int j=0;j<new_size;j++){
+                if(edges[itr].y==graphEdge_arr[j].ver) edges[itr].y_lvl=graphEdge_arr[j].weight;
+            }
+
+
+            i++;
+            itr++;
+
+        }
+
+
+        for(int k=0;k<dim;k++){
+            int connector=bananas[k];
+            int route_counter=0;
+            int max_itr=graphEdge_arr[idx].weight+1;
+            while(max_itr>0){
+                for(int i=0;i<s2/2;i++){
+                    if(edges[i].x==connector && edges[i].y_lvl>route_counter) {connector=edges[i].y;route_counter=edges[i].y_lvl;}
+
+                    else if (edges[i].y==connector && edges[i].x_lvl>route_counter)  {connector=edges[i].x;route_counter=edges[i].x_lvl;}
+
+                }
+                max_itr--;
+            }
+
+            if(route_counter==graphEdge_arr[idx].weight) {cout<<"Jest ok"<<endl;}
+            else {cout<<"NIE JEST OK!!!!!!11"<<endl;}
+        }
+
+
 }
 
 
